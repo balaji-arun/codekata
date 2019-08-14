fcfd96e39f7d0b75bc9aac6d2b9b50a90f418e81
balaji-arun <53910355+balaji-arun@users.noreply.github.com>
2019-08-14 16:58:30 +0530

    Create player_set1_problem1.cpp

---
+++ player_set1_problem1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+
+void reverse(string str) 
+{ 
+   for (int i=str.length()-1; i>=0; i--) 
+      cout << str[i];  
+} 
+  
+
+int main(void) 
+{ 
+    string s ;
+    cin>>s;
+    reverse(s); 
+    return (0); 
+} 
