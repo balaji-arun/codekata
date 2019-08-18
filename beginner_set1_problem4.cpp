ca6d10cb1660296d78da7624f32d6c76a8b73bdb
balaji-arun <53910355+balaji-arun@users.noreply.github.com>
2019-08-18 11:24:12 +0530

    Create beginner_set1_problem4.cpp

---
+++ beginner_set1_problem4.cpp
@@ -0,0 +1,20 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char c;
+   
+  
+    cin >> c;
+    if (c < 'a' || c > 'z'){
+        cout<<"No";
+       
+    }
+    else 
+        cout << " Alphabet";
+    return 0;
+}
